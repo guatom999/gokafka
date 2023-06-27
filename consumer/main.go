216027fab7d807f86309807b69c981a7ec096c6d
guatom999 <35306045+guatom999@users.noreply.github.com>
2023-06-27 23:59:35 +0700

    update consumer

--- consumer/main.go
+++ consumer/main.go
@@ -1,40 +1,72 @@
 package main
 
 import (
-	"fmt"
+	"consumer/repositories"
+	"strings"
 
 	"github.com/Shopify/sarama"
+	"github.com/spf13/viper"
+	"gorm.io/gorm"
 )
+
+func init() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
 
-	server := []string{"localhost:9092"}
-
-	consumer, err := sarama.NewConsumer(server, nil)
+	consumerGroup, err := sarama.NewConsumerGroup(viper.GetStringSlice("kafka.server"), viper.GetString("kafka.gro"), nil)
 
 	if err != nil {
 		panic(err)
 	}
 
-	defer consumer.Close()
+	var db *gorm.DB
 
-	partitionConsumer, err := consumer.ConsumePartition("bosshello", 0, sarama.OffsetNewest)
+	chon := repositories.NewAccountRepositoryDB(db)
 
-	if err != nil {
-		panic(err)
-	}
+	defer consumerGroup.Close()
 
-	defer partitionConsumer.Close()
-
-	fmt.Print("Consumer start.")
-
-	for {
-		select {
-		case err := <-partitionConsumer.Errors():
-			fmt.Println(err)
-		case msg := <-partitionConsumer.Messages():
-			fmt.Println(string(msg.Value))
-		}
-	}
+	// consumerGroup.Consume()
 
 }
+
+// func main() {
+
+// 	server := []string{"localhost:9092"}
+
+// 	consumer, err := sarama.NewConsumer(server, nil)
+
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	defer consumer.Close()
+
+// 	partitionConsumer, err := consumer.ConsumePartition("bosshello", 0, sarama.OffsetNewest)
+
+// 	if err != nil {
+
+// 	defer partitionConsumer.Close()
+
+// 	fmt.Print("Consumer start.")
+
+// 	for {
+// 		select {
+// 		case err := <-partitionConsumer.Errors():
+// 			fmt.Println(err)
+// 		case msg := <-partitionConsumer.Messages():
+// 			fmt.Println(string(msg.Value))
+// 		}
+// 	}
+
+// }
---
+++ consumer/repositories/account.go
@@ -0,0 +1,43 @@
+package repositories
+
+import "gorm.io/gorm"
+
+type BankAccount struct {
+	ID            string
+	AccountHolder string
+	AccountType   string
+	Balance       float64
+}
+
+type AccountRepository interface {
+	Save(bankAccount BankAccount) error
+	Delete(id string) error
+	FindAll() (bankAccount []BankAccount, err error)
+	FindById(id string) (bankAccount BankAccount, err error)
+}
+
+type accountRepository struct {
+	db *gorm.DB
+}
+
+func NewAccountRepositoryDB(db *gorm.DB) AccountRepository {
+	db.Table("banks").AutoMigrate(&BankAccount{})
+	return accountRepository{db}
+	// return AccountRepository2{db}
+}
+
+func (obj accountRepository) Save(bankAccount BankAccount) error {
+	return obj.db.Save(bankAccount).Error
+}
+
+func (obj accountRepository) Delete(id string) error {
+	return obj.db.Table("banks").Where("id=?", id).Delete(&BankAccount{}).Error
+}
+
+func (obj accountRepository) FindAll() (bankAccounts []BankAccount, err error) {
+	err := obj.db.Table("banks").Find(&bankAccounts).Error
+}
+
+func (obj accountRepository) FindById(id string) (bankAccount BankAccount, err error) {
+	return bankAccount, nil
+}
